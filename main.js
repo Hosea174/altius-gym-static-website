634496c302dd4a797b4865151b2befe204a99c55
Hosea174 <hossaena2018@gmail.com>
2023-06-16 00:32:34 +0300

    some hero section styling

--- main.js
+++ main.js
@@ -2,14 +2,17 @@ const body = document.querySelector("body");
 const header = document.querySelector("header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const nav = document.querySelector(".navigation");
+const menu = document.querySelector(".menu");
 
 navToggle.addEventListener("click", () => {
   if (nav.hasAttribute("data-visible")) {
     nav.setAttribute("aria-expanded", false);
     body.style.overflow = "auto";
+    menu.src = "resources/menu-open.svg";
   } else {
     nav.setAttribute("aria-expanded", true);
     body.style.overflow = "hidden";
+    menu.src = "resources/menu-close.svg";
   }
   nav.toggleAttribute("data-visible");
   header.toggleAttribute("data-overlay");
