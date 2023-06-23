a78cebe86fb2f7d344a5ebf98bc56c3193231e63
Hosea174 <hossaena2018@gmail.com>
2023-06-24 02:29:44 +0300

    added scroll animations

--- main.js
+++ main.js
@@ -35,10 +35,66 @@ const slider2 = new A11YSlider(document.querySelector(".slider2"), {
   arrows: false,
   SlidesToShow: 1,
   autoplay: true,
-  autoplaySpeed: 2500,
+  autoplaySpeed: 3000,
   // responsive: {
   //   770: {
   //     arrows: true,
   //   },
   // }
+});
+
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach ((entry) => {
+//     console.log(entry)
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add('show');
+//       entry.target.classList.add('');
+//     } else {
+//     entry.target.classList.remove('show');
+//     }
+//   });
+// });  
+
+// const hiddenElements = document.querySelectorAll('.hidden') ;
+// hiddenElements.forEach((el) => observer.observe(el));
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      if (entry.target.classList.contains("slide-right")) {
+        entry.target.classList.add("slide-right-appear");
+      } else if (entry.target.classList.contains("slide-left")) {
+        entry.target.classList.add("slide-left-appear");
+      } else if (entry.target.classList.contains("slide-up")) {
+        entry.target.classList.add("slide-up-appear");
+      }
+    } else {
+      if (entry.target.classList.contains("slide-right")) {
+        entry.target.classList.remove("slide-right-appear");
+      } else if (entry.target.classList.contains("slide-left")) {
+        entry.target.classList.remove("slide-left-appear");
+      } else if (entry.target.classList.contains("slide-up")) {
+        entry.target.classList.remove("slide-up-appear");
+      }
+    }
+  });
+});
+
+const slideRightElements = document.querySelectorAll(".slide-right");
+slideRightElements.forEach((el) => {
+  el.classList.add("hide-left");
+  observer.observe(el);
+});
+
+const slideLeftElements = document.querySelectorAll(".slide-left");
+slideLeftElements.forEach((el) => {
+  el.classList.add("hide-right");
+  observer.observe(el);
+});
+
+const slideUpElements = document.querySelectorAll(".slide-up");
+slideUpElements.forEach((el) => {
+  el.classList.add("hide-down");
+  observer.observe(el);
 });
